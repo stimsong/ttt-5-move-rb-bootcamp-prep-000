74cfd3630c4ac608957b023674f13f0332bb9ca5
Greg Stimson <stimsong@yahoo.com>
2018-02-03 19:24:47 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -13,6 +13,6 @@ def input_to_index(user_input)
   converted_input = converted_input - 1
 end
 
-def move(board, players_move, players_character)
+def move(board, players_move, players_character=" ")
   display_board(board[players_move] = players_character)
 end
